cbe36c447d393322751ad23212664cdea1ea487d
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 16:52:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_the_cheese(contains_cheddar)# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 
  if contains_cheddar.any? { |i| cheese_types.include? i }
-   return
+   return contains_cheddar
  else
    return nil
  end
