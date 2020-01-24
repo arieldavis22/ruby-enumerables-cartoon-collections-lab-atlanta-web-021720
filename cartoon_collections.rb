4c7e8ff96df228b1176f269c202b2b9c86b677b8
Ariel Davis <aadavisbusiness@gmail.com>
2020-01-24 16:47:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,5 +21,5 @@ def find_the_cheese(contains_cheddar)# code an argument here
   #binding.pry
   cheese_types = ["cheddar", "gouda", "camembert"]
 
-  contains_cheddar.any? { |i| cheese_types.include? i  }
+  if contains_cheddar.any? { |i| cheese_types.include? i p i }
 end
